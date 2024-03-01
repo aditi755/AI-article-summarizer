a35bdfab2f6bcb0c966c88f05e72bfedb7d0123d
Aditi Bhadoriya <awdhesh1700@gmail.com>
2024-03-01 19:10:26 +0530

    main features added

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,22 @@
 import React, { useState } from 'react'
+import Hero from './components/Hero'
+import Demo from './components/Demo'
 import './App.css'
 
 function App() {
 
   return (
-    <>
-       Hii
-    </>
+    <main>
+      <div className="main">
+        <div className="gradient" />
+        </div>
+        <div className="app">
+          
+          <Hero />
+          <Demo />
+        </div>
+    
+    </main>
   )
 }
 
